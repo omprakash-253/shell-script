ab49e71810db85becadca496afd3c5ad8f119d24
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 12:02:28 +0530

    added 02-variables.sh

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh how are you doing?"
+echo "Suresh:: Hello Ramesh, I'm doing good, how do you do?"
+echo "Ramesh:: I'm good too, how is your work?"
+echo "Suresh:: Not bad, I'm thinkg to upgrade to Devops"
