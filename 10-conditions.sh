cce7af0334b2990f41b43f1b140a9167f0d7ff06
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 15:47:13 +0530

    added 10-conditions.sh

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,11 @@
 
 number=$1
 
-if [ $number -gt 10 ]  # space is mandoatory in the square brakets
+if [ $number -ge 10 ]  # space is mandoatory in the square brakets
 then
     echo "Given $number is greater than 10"
 else
     echo "Given $number is lesser than 10"
 fi
+
+# -gt for greater than, -ge -> greater than and equals, -eq -> equals to, lt -> lessthan, -le -> lessthan equals to 
