1f238ac29e0d64c2795e0b6217f8369c8c09dac6
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 15:03:13 +0530

    added 09-special-variables.sh

--- 09-Special-variables.sh
+++ 09-Special-variables.sh
@@ -3,3 +3,4 @@
  echo "All variables:$@" # $@ to list all supplied variables
  echo "number of variables passed:$#" # $# to list total number of variables passed
  echo "Script name is:$0" # to display the current script name
+ echo "current working directory is: $PWD"  #$PWD to display the current working directory
