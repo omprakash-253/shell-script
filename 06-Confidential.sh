886862bcacaa0bd23e00391fa58f32101b5b2bf3
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 12:53:02 +0530

    code updated to 06-confidential.sh

--- 06-Confidential.sh
+++ 06-Confidential.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-read -p "Please enter username:" USERNAME
+read -s -p "Please enter username:" USERNAME
 
-read -p "Please enter password:" PASSWORD  
+read -s -p "Please enter password:" PASSWORD  
 
 echo "username is $USERNAME & password is $PASSWORD"
 
-# in above script, next to read "-p" denotes prompt for username & password.
+# in above script, next to read "-p" denotes prompt for username & password. 
+# -s to hide unsername & password while entering.
 # we have achived what we did in 05-Confidential.sh in this script with less lines of code.
+# remove line 7 (echo) in real time, we included here just for testing to see username & password.
 
