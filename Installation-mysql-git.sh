a1f98e2cc3a2be864200e3c9874e5d579a964c3e
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 16:14:09 +0530

    added Installation-mysql-git.sh

--- Installation-mysql-git.sh
+++ Installation-mysql-git.sh
@@ -5,15 +5,15 @@ userid=$(id -u)
 if [ $userid -ne 0 ] 
 then
     echo "Please Run the script with root user"
-    EXIT 1 # manully exit other than 0 ,here 0 means success if previous command
+    exit 1 # manully exit other than 0 ,here 0 means success if previous command
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]  # $? checks the previous command output, it its 0 means success.
 then
     echo " Installation of mysql.. FAILURE, check log for errors to troubleshoot"
-    EXIT 1
+    exit 1
 fi
 
 dnf install git -y
@@ -21,5 +21,5 @@ dnf install git -y
 if [ $? -eq 0 ]
 then 
     echo "Installation of git .. Failure, check logs for erros to troubleshoot"
-    EXIT 1
+    exit 1
 fi
