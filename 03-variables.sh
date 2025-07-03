90772e7b8a16c5c9d4b9b906b0d1454fe3a393f5
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 12:06:54 +0530

    added 03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Declare variables 
+person1=Ramesh
+person2=Suresh
+
+echo "$person1:: Hello $person2 how are you doing?"
+echo "$person2:: Hello $person1, I'm doing good, how do you do?"
+echo "$person1:: I'm good too, how is your work?"
+echo "$person2:: Not bad, I'm thinkg to upgrade to Devops"
