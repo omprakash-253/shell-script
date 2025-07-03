90a5e11565c4063c86c517468ca1bc782e9d7236
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 15:06:24 +0530

    added 09-special-variables.sh

--- 09-Special-variables.sh
+++ 09-Special-variables.sh
@@ -5,3 +5,4 @@
  echo "Script name is:$0" # to display the current script name
  echo "current working directory is: $PWD"  #$PWD to display the current working directory
  echo "Home directory of current user: $HOME" # to display the current user home directory
+ echo "Which user is running the script: $USER" # to diplay which user is running the script
