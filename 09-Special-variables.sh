3862fc70993253853c251b959045f8a26a1e6bf9
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 15:02:03 +0530

    added 09-special-variables.sh

--- 09-Special-variables.sh
+++ 09-Special-variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
  
  echo "All variables:$@" # $@ to list all supplied variables
- echo "number of variables passed:$#"
+ echo "number of variables passed:$#" # $# to list total number of variables passed
+ echo "Script name is:$0" # to display the current script name
