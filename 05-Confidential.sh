b2e74cfd93e98355a319a33dcfc25f8e31e21103
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 12:19:15 +0530

    amended comments to 05-confidential.sh

---
+++ 05-Confidential.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read USERNAME  # here USERNAME is variable
+
+echo "Please enter password::"
+read PASSWORD
+
+echo "username is $USERNAMR & password is $PASSWORD"
