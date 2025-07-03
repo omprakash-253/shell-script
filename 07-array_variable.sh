9833315e828acbff2ed7d29177ed8e9174e3adea
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 14:47:34 +0530

    bug fixed to get all movies on 07-array_variable.sh

--- 07-array_variable.sh
+++ 07-array_variable.sh
@@ -4,8 +4,9 @@ movies=("RRR" "DJTillu" "Murari")
 
 echo "First movie is: ${movies[0]}"
 echo "2nd movie is : ${movies[1]}"
-echo "list of all movies are : ${[@]}"
+echo "list of all movies are : ${movies[@]}"
 
 #list always starts with 0(Zero)
 #Size of the above array is 3
 # index of aboce array are 0,1,2
+# in line 7 @ represents all 
