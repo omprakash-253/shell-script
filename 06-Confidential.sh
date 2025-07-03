9fa0ff0b4a68acd6ce37cd6ae67c2020defb927f
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-03 12:48:23 +0530

    added 06-confidential.sh

---
+++ 06-Confidential.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Please enter username:" USERNAME
+
+read -p "Please enter password:" PASSWORD  
+
+echo "username is $USERNAME & password is $PASSWORD"
+
+# in above script, next to read "-p" denotes prompt for username & password.
+# we have achived what we did in 05-Confidential.sh in this script with less lines of code.
+
